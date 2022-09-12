ba080a67b0a51a69eeb4dbabd6e4e2805ef08265
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2022-09-12 13:35:11 +0000

    Update for.rkt

--- for.rkt
+++ for.rkt
@@ -5,7 +5,7 @@
 (provide for/arg*
          for/arg)
 
-(define-syntax-rule (for/arg* ([index-var index-start] value-var) body ...)
+(define-syntax-rule (for/arg* ([index-var index-start]) body ...)
   (cond [(not (exact-nonnegative-integer? index-start))
          (raise-argument-error 'for/arg* "exact-nonnegative-integer?" index-start)]
         
@@ -13,15 +13,14 @@
          (raise-range-error 'for/arg* "vector" "starting " index-start (argv) 0 (- (argv-length) 1))]
 
         [else
-         (let ([index-var index-start]
-               [value-var null])
+         (let ([index-var index-start])
            (let loop ()
+             (set! value-var (argv index-var))
              (when (< index-var (argv-length))
-               (set! value-var (argv index-var))
                body ...
                (set! index-var (add1 index-var))
                (loop))))]))
 
-(define-syntax-rule (for/arg [index-var value-var] body ...)
-  (for/arg* ([index-var 0] value-var)
+(define-syntax-rule (for/arg [index-var] body ...)
+  (for/arg* ([index-var 0])
     body ...))
